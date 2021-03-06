bbcf850507bf699e4f7247bead2925f75c8dee92
Trouni Tiet <trouni@gmail.com>
2021-03-06 22:54:18 +0900

    Add revision

--- hooks/deployed.sh
+++ hooks/deployed.sh
@@ -3,5 +3,6 @@ curl -LX POST https://enrkw4y1wo7fm.x.pipedream.net \
 -d "{
     \"type\": \"engine.deployed\",
     \"engine_slug\": \"$1\",
-    \"service_url\": \"$(gcloud run services describe $1 --format 'value(status.url)' --platform managed --region $2)\"
+    \"service_url\": \"$(gcloud run services describe $1-$2 --format 'value(status.url)' --platform managed --region $3)\",
+    \"revision\": \"$4\"
 }"
