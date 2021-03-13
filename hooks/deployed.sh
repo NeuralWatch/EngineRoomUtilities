e4f07dd070fe6f4f1b31c90b59d7dcb9357b79ea
Trouni <34345789+trouni@users.noreply.github.com>
2021-03-13 21:04:13 +0900

    Fix API url and GCR service name

--- hooks/deployed.sh
+++ hooks/deployed.sh
@@ -1,8 +1,8 @@
-curl -LX POST https://neuralwatch.herokuapp.com/webhook \
+curl -LX POST https://neuralwatch-api.herokuapp.com/webhook \
 -H 'Content-Type: text/plain' \
 -d "{
     \"type\": \"engine.deployed\",
     \"engine_slug\": \"$1\",
-    \"service_url\": \"$(gcloud run services describe $1-$2 --format 'value(status.url)' --platform managed --region $3)\",
+    \"service_url\": \"$(gcloud run services describe aiengine-$1-api-$2 --format 'value(status.url)' --platform managed --region $3)\",
     \"revision\": \"$4\"
 }"
