b408af9da10afa86362c7a52212750b2fb7841b7
Trouni <34345789+trouni@users.noreply.github.com>
2021-04-04 21:53:22 +0900

    Update deployed.sh

--- hooks/deployed.sh
+++ hooks/deployed.sh
@@ -3,6 +3,6 @@ curl -LX POST https://neuralwatch-api.herokuapp.com/webhook \
 -d "{
     \"type\": \"engine.deployed\",
     \"engine_slug\": \"$1\",
-    \"service_url\": \"$(gcloud run services describe aiengine-$1-api-$2 --format 'value(status.url)' --platform managed --region $3)\",
+    \"service_url\": \"$(gcloud run services describe engine-$1-$2 --format 'value(status.url)' --platform managed --region $3)\",
     \"revision\": \"$4\"
 }"
